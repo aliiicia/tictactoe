89ae4281a1746b6ce1f1a0011435b9ef6cd20f2d
Alicia Tan <z5316897@ad.unsw.edu.au>
2023-05-10 19:58:08 +1000

    wow button

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Box } from './components/Box'
+
+function App() {
+  const board = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+  return (
+    <div className="App">
+      <Box value='X' onClick={null} />
+    </div>
+  );
+}
+
+export default App;
